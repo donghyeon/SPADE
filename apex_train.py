f2d4a1751d895613c5ab802722e6a50767eca9fb
donghyeon <donghyeonyi@gmail.com>
2020-03-23 18:36:02 +0900

    Visualize only for rank 0 of DDP process

--- apex_train.py
+++ apex_train.py
@@ -72,24 +72,25 @@ for epoch in iter_counter.training_epochs():
         trainer.run_discriminator_one_step(data_i)
 
         # Visualizations
-        if iter_counter.needs_printing():
-            losses = trainer.get_latest_losses()
-            visualizer.print_current_errors(epoch, iter_counter.epoch_iter,
-                                            losses, iter_counter.time_per_iter)
-            visualizer.plot_current_errors(losses, iter_counter.total_steps_so_far)
+        if opt.local_rank == 0:
+            if iter_counter.needs_printing():
+                losses = trainer.get_latest_losses()
+                visualizer.print_current_errors(epoch, iter_counter.epoch_iter,
+                                                losses, iter_counter.time_per_iter)
+                visualizer.plot_current_errors(losses, iter_counter.total_steps_so_far)
 
-        if iter_counter.needs_displaying():
-            visuals = OrderedDict([('input_label', data_i['label']),
-                                   ('synthesized_image', trainer.get_latest_generated()),
-                                   ('real_image', data_i['image'])])
-            visualizer.display_current_results(visuals, epoch, iter_counter.total_steps_so_far,
-                                               iter_counter.epoch_iter)
+            if iter_counter.needs_displaying():
+                visuals = OrderedDict([('input_label', data_i['label']),
+                                       ('synthesized_image', trainer.get_latest_generated()),
+                                       ('real_image', data_i['image'])])
+                visualizer.display_current_results(visuals, epoch, iter_counter.total_steps_so_far,
+                                                   iter_counter.epoch_iter)
 
-        if iter_counter.needs_saving():
-            print('saving the latest model (epoch %d, total_steps %d)' %
-                  (epoch, iter_counter.total_steps_so_far))
-            trainer.save('latest')
-            iter_counter.record_current_iter()
+            if iter_counter.needs_saving():
+                print('saving the latest model (epoch %d, total_steps %d)' %
+                      (epoch, iter_counter.total_steps_so_far))
+                trainer.save('latest')
+                iter_counter.record_current_iter()
 
     trainer.update_learning_rate(epoch)
     iter_counter.record_epoch_end()
